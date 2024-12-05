ab9c8e6be1bd54834c3f940c558608acc7913c58
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-12-06 00:11:08 +0300

    day01

--- src/test/java/day01/C02_TestNotation.java
+++ src/test/java/day01/C02_TestNotation.java
@@ -1,7 +1,37 @@
 package day01;
 
+import org.junit.jupiter.api.*;
+
 public class C02_TestNotation {
 
+    @BeforeAll
+    public static void setup() {
+        System.out.println("Test Sureci baslatiliyor");
+    }
 
+    @AfterAll
+    public static void teardown(){
+        System.out.println("Test Sureci Tamamlandi");
+    }
+
+    @BeforeEach
+    public void beforeTest(){
+        System.out.println("Siradaki Test Calistiriliyor\n");
+    }
+
+    @AfterEach
+    public void afterTest(){
+        System.out.println("\nBir Test Scope'u Tamamlandi. Varsa Diger Scope Calisacak");
+    }
+
+    @Test
+    public void zeynep(){
+        System.out.println("Zeynep Hasret Kirbas\nQA Engineer\n\n");
+    }
+
+    @Test
+    public void nazli(){
+        System.out.println("Nazli Can Kirbas\nSoftware Developer\n\n");
+    }
 
 }
--- src/test/java/day01/C03_SignInTesti.java
+++ src/test/java/day01/C03_SignInTesti.java
@@ -1,35 +1,94 @@
 package day01;
 
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C03_SignInTesti {
 
-        // 1- https://babayigit.net/test/ adresine gidin
+    static WebDriver driver;
 
+    @BeforeAll
+    public static void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void teardown() {
+        driver.quit();
+    }
+
+    @BeforeEach
+    public void girisEkrani() {
+        // 1- https://babayigit.net/test/ adresine gidin
+        driver.get("https://babayigit.net/test/");
 
         // 2- Login butonuna tıklayın
+        driver.findElement(By.xpath("//*[@*='img/login.png']"))
+                .click();
+    }
 
+    @AfterEach
+    public void testBitir() {
+        System.out.println("\nTest Tamamlandi\n");
+    }
+
+    @Test
+    public void negativeLogin() {
 
         // 3- Username kısmına “babayigit” yazdırın
-
+        driver.findElement(By.id("username"))
+                .sendKeys("babayigit");
 
         // 4- Password kısmına baba123 yazdırın
-
+        driver.findElement(By.id("password"))
+                .sendKeys("baba123");
 
         // 5- Login butonuna tıklayın
-
+        driver.findElement(By.xpath("//*[@*='button']"))
+                .click();
 
         // 6- Başarılı giriş yapılamadığını doğrulayın
+        String expectedMessage = "The username or password is incorrect.";
+        String actualMessage = driver.findElement(By.id("result")).getText();
 
+        if (actualMessage.equals(expectedMessage)) {
+            System.out.println("Giris Yapilamadi. Bilgi Hatasi Var\nThe username or password is incorrect.");
+        }
 
-        // 7- Login Sayfasında kullanıcı adına “babayigit” Pasword kısmına “Mb3471&” yazdırın
+    }
 
+    @Test
+    public void positiveLogin() {
+
+        // 7- Login Sayfasında kullanıcı adına “babayigit” Password kısmına “Mb3471&” yazdırın
+        driver.findElement(By.id("username"))
+                .sendKeys("babayigit");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("Mb3471&");
 
         // 8- Login butonuna tıklayın
-
+        driver.findElement(By.xpath("//*[@*='button']"))
+                .click();
 
         // 9- Başarılı giriş yapıldığını doğrulayın
+        String expectedMessage = "Successful entry!";
+        String actualMessage = driver.findElement(By.id("result")).getText();
 
+        if (actualMessage.equals(expectedMessage)) {
+            System.out.println("Giris Yapildi.\nSuccessful entry!");
+        }
 
-        // 10- Sayfayı kapatın
-
+    }
 
 }
+
+
